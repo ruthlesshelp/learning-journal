401b338230c060103694834c6489468891a2634b
ruthlesshelp <ruthlesshelp@gmail.com>
2013-09-04 04:54:46 -0400

    Added initial DiveIn solution

---
+++ AgileEngTraining/DiveIn/Excella.TempConv/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excella.TempConv
+{
+    public class TemperatureConverter
+    {
+        public int ToFahrenheit(int degreesCelsius)
+        {
+            if (degreesCelsius == 100) return 212;
+
+            return 32;
+        }
+
+        public int ToCelsius(int degreesFahrenheit)
+        {
+            if (degreesFahrenheit == 212) return 100;
+
+            return 0;
+        }
+    }
+}
---
+++ AgileEngTraining/DiveIn/Tests.Unit.Excella.TempConv/Class1.cs
@@ -0,0 +1,65 @@
+﻿using Excella.TempConv;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Unit.Excella.TempConv
+{
+    public class TemperatureConverterTests
+    {
+        [Test]
+        public void ToFahrenheit_WhenZeroCelsius_Expect32()
+        {
+            // Arrange
+            var converter = new TemperatureConverter();
+
+            // Act
+            var temperature = converter.ToFahrenheit(0);
+
+            // Assert
+            Assert.AreEqual(32, temperature);
+        }
+
+        [Test]
+        public void ToFahrenheit_When100Celsius_Expect212()
+        {
+            // Arrange
+            var converter = new TemperatureConverter();
+
+            // Act
+            var actual = converter.ToFahrenheit(100);
+
+            // Assert
+            Assert.AreEqual(212, actual);
+        }
+
+        [Test]
+        public void ToCelsius_When32F_Expect0C()
+        {
+            // Arrange
+            var converter = new TemperatureConverter();
+
+            // Act
+            var actual = converter.ToCelsius(32);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void ToCelsius_When212F_Expect100C()
+        {
+            // Arrange
+            var converter = new TemperatureConverter();
+
+            // Act
+            var actual = converter.ToCelsius(212);
+
+            // Assert
+            Assert.AreEqual(100, actual);
+        }
+    }
+}
